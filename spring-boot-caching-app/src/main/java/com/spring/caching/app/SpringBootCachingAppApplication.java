bd9a5d6918f46f9c59e37a13f6872c8969cb9880
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-01-09 12:28:04 +0200

    chore: Initialize project with required maven dependencies

---
+++ spring-boot-caching-app/src/main/java/com/spring/caching/app/SpringBootCachingAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.caching.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCachingAppApplication.class, args);
+    }
+
+}
---
+++ spring-boot-caching-app/src/test/java/com/spring/caching/app/SpringBootCachingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.caching.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCachingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
